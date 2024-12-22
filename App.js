f53184e45154cf612bb19fad6cf95cc88566b0a4
ManouBlog <adjobikadjopierre27@gmail.com>
2024-12-22 00:46:00 +0000

    new

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+import { StyleSheet, Text, View,Image,SafeAreaView,ScrollView, Platform } from 'react-native';
+import {dogs} from "./data"
+ const Card = ({name,description,image,size})=>{
+  return(
+    <View style={{width:360,
+      height:560,
+    backgroundColor:'gray',
+    padding:10,
+    borderRadius:30,
+    marginBottom:30}}>
+      <Image 
+      style={{width:"100%",height:420,borderRadius:20}}
+      source={{uri:image}} />
+      <Text style={{fontSize:50}}>{name}</Text>
+      <Text>{description}</Text>
+    </View>
+  )
+ }
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView 
+      contentContainerStyle={{marginHorizontal:15}}
+      style={{marginTop:Platform.OS === 'android' && 40}}>
+      {
+        dogs.map((item,index)=>(
+          <Card key={index} {...item} />
+        ))
+      }
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
